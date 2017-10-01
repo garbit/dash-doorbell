b7a171f67346bf8b95ce307c91fa529d35840d63
Andy Garbett <atgarbett@gmail.com>
2017-10-01 19:00:43 +0100

    Promisified code and added module exports

--- index.js
+++ index.js
@@ -1,60 +1,128 @@
 require('dotenv').config();
-var dash_button = require('node-dash-button');
-var dash = dash_button(process.env.DASH_MAC, null, 1000, 'all');
+const dash_button = require('node-dash-button');
+const dash = dash_button(process.env.DASH_MAC, null, 1000, 'all');
 const fs = require('fs');
 const request = require('request');
-const url = process.env.CAM_URL;
+const secureCamUrl = process.env.CAM_URL;
 const AWS = require('aws-sdk');
 const bucketName = process.env.BUCKET_NAME;
 const ifttt_url = process.env.IFTTT_URL;
 const moment = require('moment');
 
 AWS.config.loadFromPath('aws-credentials.json');
-var s3 = new AWS.S3();
+
+const s3 = new AWS.S3({signatureVersion: 'v4'});
 var s3Bucket = new AWS.S3({ params: { Bucket: bucketName } })
 
-var download = function (uri, filename, callback) {
-	request.head(uri, function (err, res, body) {
-		request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
+function download(uri, file) {
+	return new Promise(function (resolve, reject) {
+		request.head(uri, function (err, res, body) {
+			if (err) {
+				reject(err)
+			} else {
+				request(uri).pipe(fs.createWriteStream(file)).on('close', async () => {
+					let tmpFile = await readFileAsync(file);
+					resolve(tmpFile);
+				})
+			}
+		});
+	})
+	.catch(e => {
+		return e;
 	});
 };
 
-
-dash.on("detected", function () {
-	console.log("found button");
-
-	var dateTime = new Date();
-	now = moment(dateTime).format("YYYY-MM-DD HH-mm-ss");
-	let filename = now + ".png";
-
-	download(url, 'tmp/' + filename, function () {
-		fs.readFile('tmp/' + filename, function (err, data) {
-			if (err) { throw err; }
-
-			s3Bucket.putObject({
-				Key: filename,
-				Body: data,
-				ContentType: 'image/png'
-			}, function (res) {
-				var params = { Bucket: bucketName, Key: filename, Expires: 3600 };
-
-				let url = s3.getSignedUrl('getObject', params);
-
-				console.log(url);
-
-				fs.unlink('tmp/' + filename, function (error) {
-					if (error)
-						console.log("Error removing file");
-					request(ifttt_url, {
-						method: 'POST',
-						json: true,
-						body: {
-							value1: url
-						}
-					});
-				});
-			});
+function readFileAsync(path) {
+	return new Promise(function (resolve, reject) {
+		fs.readFile(path, function (error, result) {
+			if (error) {
+				reject(error);
+			} else {
+				resolve(result);
+			}
 		});
+	})
+	.catch(e => {
+		return e;
 	});
+};
+
+function unlinkFileAsync(path) {
+	return new Promise(function (resolve, reject) {
+		fs.unlink(path, function (error) {
+			if (error) {
+				reject(error);
+			} else {
+				resolve(true);
+			}
+		});
+	})
+	.catch(e => {
+		return e;
+	});
+}
+
+function putObjectAndGetSignedUrlAsync(key, body, contentType) {
+	return new Promise(function (resolve, reject) {
+		s3Bucket.putObject({
+			Key: key,
+			Body: body,
+			ContentType: contentType
+		}, async function (error, res) {
+			if (error) {
+				reject(error);
+			} else {
+				var params = { Bucket: bucketName, Key: key, Expires: 3600 };
+				let url = s3.getSignedUrl('getObject', params);
+				resolve(url);
+			}
+		});
+	})
+	.catch(e => {
+		return e;
+	});
+};
+
+dash.on("detected", async function () {
+
+	try {
+		console.log("found button");
+
+		// create new filename
+		var dateTime = new Date();
+		now = moment(dateTime).format("YYYY-MM-DD HH-mm-ss");
+
+		let filename = now + ".png";
+		let file = 'tmp/' + filename;
+
+		// get latest image from picam
+		tempFile = await download(secureCamUrl, file);
+
+		// get signed S3 url
+		signedUrl = await putObjectAndGetSignedUrlAsync(filename, tempFile, 'image/png');
+		console.log(signedUrl);
+
+		// remove .tmp file
+		await unlinkFileAsync(file);
+
+		// send IFTTT notification
+		request(ifttt_url, {
+			method: 'POST',
+			json: true,
+			body: {
+				value1: signedUrl
+			}
+		});
+	}
+	catch (e) {
+		console.log(e);
+	}
 });
+
+module.exports = {
+	download: download,
+	readFileAsync: readFileAsync,
+	unlinkFileAsync: unlinkFileAsync,
+	putObjectAndGetSignedUrlAsync: putObjectAndGetSignedUrlAsync
+}
 
